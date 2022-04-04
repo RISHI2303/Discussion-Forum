61e655127692a78208a5a8566f1b32c5eb8cea73
Rishi Goyal <rishigoyal230@gmail.com>
2022-04-04 23:52:57 +0530

    added hide and show question details

--- discuss.js
+++ discuss.js
@@ -2,6 +2,15 @@ let submitQuestionNode = document.getElementById('submitBtn');
 let questionTitleNode = document.getElementById('subject');
 let questionDescriptionNode = document.getElementById('question');
 let allQuestionsListNode = document.getElementById('dataList');
+let createQuestionNode = document.getElementById("toggleDisplay");
+let questionDetailContainerNode = document.getElementById("respondQue");
+let resolveQuestionContainerNode = document.getElementById("resolveHolder");
+let resolveQuestion = document.getElementById("resolveQuestion");
+let responseContainerNode = document.getElementById("respondAns");
+let commentContainerNode = document.getElementById("commentHolder");
+let commentatorNameNode = document.getElementById("pickName");
+let commentNode = document.getElementById("pickComment");
+let submitCommentNode = document.getElementById("commentBtn");
 
 // display all existing questions
 function onLoad() {
@@ -15,9 +24,7 @@ function onLoad() {
 onLoad();
 
 // listen for submit button to create a new question
-
 submitQuestionNode.addEventListener('click', onQuestionSubmit);
-
 function onQuestionSubmit() {
     let question;
     if (questionTitleNode.value != null || questionDescriptionNode.value != null) {
@@ -38,7 +45,6 @@ function clearQuestionForm() {
 }
 
 // save question to storage
-
 function saveQuestion(question) {
     // get all questions from storage first and push new question to it
     // then save it back to storage
@@ -51,9 +57,9 @@ function saveQuestion(question) {
 }
 
 // append question to the left panel
-
 function addQuestionToPanel(question) {
     let questionContainer = document.createElement('div');
+    questionContainer.style.background = "grey";
 
     let newQuestionTitleNode = document.createElement('h4');
     newQuestionTitleNode.innerHTML = question.title;
@@ -64,6 +70,8 @@ function addQuestionToPanel(question) {
     questionContainer.appendChild(newQuestionDescriptionNode);
 
     allQuestionsListNode.appendChild(questionContainer);
+
+    questionContainer.addEventListener('click', onQuestionClick(question));
 }
 
 // get all functions from storage
@@ -80,19 +88,41 @@ function getAllQuestions() {
 }
 
 // listen for click on question and display in right panel
+function onQuestionClick(question) {
 
-function onQuestionClick() {
+    // through closure, we can access question object
+    return function () { 
+        // hide question panel
+        hideQuestionPanel();
 
+        // show clicked question
+        showDetails();
+
+        // create question details
+        addQuestionToRight(question);
+    }
 }
 
 // listen for click on submit response button
-
 function onResponseSubmit() {
 
 }
 
 // display response in response section
-
 function addResponseInPanel() {
 
 }
+
+// hide question panel
+function hideQuestionPanel() {
+    createQuestionNode.style.display = "none";
+}
+
+// show question panel
+function showDetails() {
+    questionDetailContainerNode.style.display = "block";
+    resolveQuestionContainerNode.style.display = "block";
+    responseContainerNode.style.display = "block";
+    commentContainerNode.style.display = "block";
+}
+
